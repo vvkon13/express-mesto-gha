c778885c483912e304f6b4dc64786cd928ce9e49
Vasiliy Konovalov <vvkon13@gmail.com>
2023-07-27 13:09:31 +0400

    Практическая работа №14 v1.0.17

--- middlewares/auth.js
+++ middlewares/auth.js
@@ -1,20 +1,21 @@
 const jwt = require('jsonwebtoken');
-const { ERROR_CODE_INCORRECT_EMAIL_PASSWORD, JWT_SECRET } = require('../utils/constants');
+const { JWT_SECRET } = require('../utils/constants');
+const IncorrectEmailPasswordError = require('../errors/IncorrectEmailPasswordErr');
 
 module.exports = (req, res, next) => {
   const { authorization } = req.headers;
   if (!authorization || !authorization.startsWith('Bearer ')) {
-    return res.status(ERROR_CODE_INCORRECT_EMAIL_PASSWORD).send({ message: 'Необходима авторизация' });
+    const e = new IncorrectEmailPasswordError('Неправильные почта или пароль');
+    next(e);
   }
   const token = authorization.replace('Bearer ', '');
   let payload;
   try {
     payload = jwt.verify(token, JWT_SECRET);
   } catch (err) {
-    return res
-      .status(ERROR_CODE_INCORRECT_EMAIL_PASSWORD).send({ message: 'Необходима авторизация' });
+    const e = new IncorrectEmailPasswordError('Неправильные почта или пароль');
+    next(e);
   }
   req.user = payload;
   next();
-  return undefined;
 };
